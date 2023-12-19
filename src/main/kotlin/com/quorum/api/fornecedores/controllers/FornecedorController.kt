935bef623f8064c1c7770459bbaa3bfdc3d3f47d
Thiago Barbosa <thiago@acamp.com>
2023-12-19 20:25:38 +0100

    adding metatag to fornecedor controller

--- src/main/kotlin/com/quorum/api/fornecedores/controllers/FornecedorController.kt
+++ src/main/kotlin/com/quorum/api/fornecedores/controllers/FornecedorController.kt
@@ -2,6 +2,12 @@ package com.quorum.api.fornecedores.controllers
 
 import com.quorum.api.fornecedores.modelos.Fornecedor
 import com.quorum.api.fornecedores.servicos.ServicoFornecedor
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,14 +20,50 @@ import org.springframework.web.bind.annotation.RestController
 class FornecedorController(
     private val servicoFornecedor: ServicoFornecedor
 ) {
+    @Operation(
+        summary = "Listar fornecedores",
+        description = "Lista todos os fornecedores.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Sucesso",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = Fornecedor::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Acesso negado"
+            )
+        ],
+        parameters = [
+            Parameter(`in` = ParameterIn.HEADER, name = "token", description = "Token de acesso", required = true, schema = Schema(type = "string"))
+        ]
+    )
     @GetMapping
     fun getAllFornecedores(): List<Fornecedor> {
         return servicoFornecedor.obterTodosFornecedores()
     }
 
+    @Operation(
+        summary = "Obter fornecedor",
+        description = "Obtem fornecedor pelo CNPJ.",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Sucesso",
+                content = [Content(mediaType = "application/json", schema = Schema(implementation = Fornecedor::class))]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Acesso negado"
+            )
+        ],
+        parameters = [
+            Parameter(`in` = ParameterIn.HEADER, name = "token", description = "Token de acesso", required = true, schema = Schema(type = "string"))
+        ]
+    )
     @GetMapping("/{cnpj}")
     fun obterFornecedorPorCnpj(
-        @PathVariable cnpj: String
+        @Parameter(description = "Codigo do CNPJ (sem pontos ou tracos") @PathVariable cnpj: String
     ): Fornecedor {
         return servicoFornecedor.obterFornecedorPorCnpj(cnpj) ?: throw Exception("Fornecedor não encontrado")
     }
